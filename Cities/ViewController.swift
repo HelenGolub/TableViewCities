9dccd1173cf44071a03c3cc74dd15a24349dd79a
Helen Golub <helensgolub@gmail.com>
2017-10-25 19:58:30 +0300

    Create TableView with yellow/white cells

--- Cities/ViewController.swift
+++ Cities/ViewController.swift
@@ -1,15 +1,13 @@
-//
-//  ViewController.swift
-//  Cities
-//
-//  Created by Елена Ярцева on 23.10.17.
-//  Copyright © 2017 com.test. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    private let citiesArray = ["Kyiv", "Lviv", "Barcelona", "Rome", "Madrid"]
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +17,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citiesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell")!
+        cell.textLabel?.text = citiesArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (indexPath.row%2 == 0) {
+            let color: UIColor = UIColor.yellow
+            cell.backgroundColor = color
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+    
 
 }
 
